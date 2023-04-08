2f60eb32d6bc956f18eb54b6a9b72d392b98625c
SOURAV MANDAL <sourav0200@gmail.com>
2023-04-09 00:52:06 +0600

    Code done

---
+++ PlusMinus.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PlusMinus {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                positiveCount++;
+            } else if (arr[i] < 0) {
+                negativeCount++;
+            } else {
+                zeroCount++;
+            }
+        }
+        double positiveRatio = (double) positiveCount / n;
+        double negativeRatio = (double) negativeCount / n;
+        double zeroRatio = (double) zeroCount / n;
+        System.out.printf("%.6f\n", positiveRatio);
+        System.out.printf("%.6f\n", negativeRatio);
+        System.out.printf("%.6f\n", zeroRatio);
+    }
+}
